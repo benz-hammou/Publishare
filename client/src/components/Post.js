068f934654681fdd216395d7913b4e3872d43f1a
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2023-12-17 21:58:40 +0100

    Use prod api endpoint

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -14,7 +14,9 @@ const Post = () => {
 
   const getPosts = async () => {
     try {
-      const res = await fetch("https://api-7niz.onrender.com/post");
+      const res = await fetch("https://api-7niz.onrender.com/post",{
+        mode:"no-cors"
+      });
       const data = await res.json();
       console.log(data);
       return setPosts(data);
