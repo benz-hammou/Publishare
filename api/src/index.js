9143e2407e7dc59e6b71885e6e63fef8325466f1
benz-hammou <Mohamed.Benhammo@student.intecbrussel.be>
2024-01-16 14:56:10 +0100

    cookie ax

--- api/src/index.js
+++ api/src/index.js
@@ -65,7 +65,7 @@ app.post("/login", async (req, res) => {
           .cookie("token", token, {
             sameSite: "none",
             secure: true,
-            maxAge: 3600000 * 10,
+            maxAge: 360000000,
           })
           .json({
             id: userDoc._id,
